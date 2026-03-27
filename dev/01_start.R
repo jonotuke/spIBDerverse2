4ac34bdfc2fb2f21f6f2f428d8e5014d395d04e3
Jonathan Tuke <jonotuke@gmail.com>
2026-03-28 07:14:43 +1030

    Set up the files

--- dev/01_start.R
+++ dev/01_start.R
@@ -17,10 +17,6 @@ golem::use_utils_server(with_test = TRUE)
 ## Use git ----
 usethis::use_git()
 ## Sets the remote associated with 'name' to 'url'
-usethis::use_git_remote(
-  name = "origin",
-  url = "https://github.com/<OWNER>/<REPO>.git"
-)
 
 # You're now set! ----
 
